568054df17d407270e37527b84323eaef24f46db
Daniel Lucas <daniellucas382@gmail.com>
2018-09-03 19:35:50 -0300

    Criação de Restaurantes Dinâmicos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AbautComponent } from './about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 export const ROUTES: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'about', component: AbautComponent}
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AbautComponent },
+    { path: 'restaurants', component: RestaurantsComponent }
 ];
